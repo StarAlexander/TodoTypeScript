2bbd50db19be9666bab2e98c30a538da0ec9115a
alexander <starchenckoal@yandex.ru>
2023-06-22 20:50:02 +0300

    bugs

--- src/CheckBox.tsx
+++ src/CheckBox.tsx
@@ -7,8 +7,7 @@ type props={
 }
 export function CheckBox({dispatch,corr,index}:props):JSX.Element{
     const [checked,setChecked]=useState(false)
-    return (<div style={{position:'relative', left:'0px', width:'4rem'}}><input onChange={()=>{
-       setChecked(prev=>!prev)
+    return (<div style={{position:'relative', left:'0px', width:'4rem'}}><input onClick={()=>setChecked(prev=>!prev)} onChange={()=>{
         dispatch(prev=>!checked?[...prev,[corr,index]]:[...prev.filter((el)=>el[0]!==corr || el[1]!==index)])}}
          type="checkbox" style= {{position:'relative',  height:'2rem', width:'1rem', left:'1.3rem', top:'0.75rem', paddingTop:'1rem'}} /></div>)
 }
